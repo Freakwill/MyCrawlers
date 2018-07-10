7084705c11b8af385b349efd2ad0bd36fe9f6206
William Song <30965609+Freakwill@users.noreply.github.com>
2018-07-10 14:53:32 +0800

    Add files via upload

--- zol/icebox.py
+++ zol/icebox.py
@@ -9,46 +9,58 @@ import base
 import product
 
 
-name_rx = re.compile(r'(?P<name>\w+ ?\w+((-|\/)\w+)*)(?P<anothername>\(\w+\))? +(?P<description>\w+)')
+class IceBoxes(product.Product):
+    pages = 15
+    
+    @staticmethod
+    def read_url(URL):
+        iceboxes = []
+        for k in range(1, pages):
+
+            ICEBOX = HOME + "/icebox/%d.html" % k
+            try:
+                soup = base.url2soup(ICEBOX)
+            except:
+                break
+
+            prods = soup.find('ul', {'id': 'J_PicMode'})
+            for prod in prods.find_all('li'):
+                h3 = prod.find('h3')
+                if h3:
+                    href = prod.find('a', {'class': 'pic'})['href']
+                    t = name_rx.match(h3.a.text.strip())
+                    name, description = t['name'], t['description']
+                    score = prod.find('span', {'class': 'score'}).text
+                    if score.isdigit():
+                        score = float(score)
+                    else:
+                        score = 0
+                    comment_num = prod.find('a', {'class': 'comment-num'}).text.strip()
+                    comment_num = int(base.digit_rx.match(comment_num)[0])
+                    price = prod.find('b', {'class': 'price-type'}).text
+                    if price.endswith('万'):
+                        price = int(float(base.decimal_rx.search(price)[0]) * 10000)
+                    elif price.isdigit():
+                        price = int(price)
+                    else:
+                        continue
+                    p = product.Product(name, description, price, comment_num, href)
+                    soup2 = base.url2soup(HOME + p.href)
+                    a = soup2.find('a', {'class': '_j_MP_more section-more'})
+                    soup2 = base.url2soup(HOME + a.get('href'))
+                    paras = soup2.find('div', {'class': 'detailed-parameters'})
+                    p.parameter = {para.find('th').text:para.find('td').find('span').text
+                    for table in paras.find_all('table')[:-1] for para in table.find_all('tr')[1:]}
+                    iceboxes.append(p)
+        return iceboxes
+                    
+
+
+name_rx = re.compile(r'(?P<name>\w+ ?\w+((-|\/)\w+)*)(?P<anothername>\(|（\w+(\)|）)?)? *(?P<description>\w+)')
+
 HOME = "http://detail.zol.com.cn"
 
-iceboxes = []
-for k in range(1, 5):
-
-    ICEBOX = HOME + "/icebox/%d.html" % k
-
-    soup = base.url2soup(ICEBOX)
-
-    prods = soup.find('ul', {'id': 'J_PicMode'})
-    for prod in prods.find_all('li'):
-        h3 = prod.find('h3')
-        if h3:
-            href = prod.find('a', {'class': 'pic'})['href']
-            t = name_rx.match(h3.a.text.strip())
-
-            name, description = t['name'], t['description']
-            score = prod.find('span', {'class': 'score'}).text
-            if not score:
-                score = 0
-            else:
-                score = float(score)
-            comment_num = prod.find('a', {'class': 'comment-num'}).text.strip()
-            comment_num = int(base.digit_rx.match(comment_num)[0])
-            price = prod.find('b', {'class': 'price-type'}).text
-            if price.endswith('万'):
-                price = int(float(base.decimal_rx.search(price)[0]) * 10000)
-            else:
-                price = int(price)
-            p = product.Product(name, description, price, comment_num, href)
-            soup2 = base.url2soup(HOME + p.href)
-            a = soup2.find('a', {'class': '_j_MP_more section-more'})
-            soup2 = base.url2soup(HOME + a.get('href'))
-            paras = soup2.find('div', {'class': 'detailed-parameters'})
-            p.parameter = {para.find('th').text:para.find('td').find('span').text
-            for table in paras.find_all('table')[:-1] for para in table.find_all('tr')[1:]}
-            iceboxes.append(p)
-
+iceboxes = IceBoxes.read_url(HOME)
 df =pd.DataFrame([p.toDict() for p in iceboxes])
 df.to_excel('iceboxes.xls')
-
 
